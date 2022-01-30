51562adaec7de3ff140a3323e8099211c21717cd
Pierre-Louis Sergent <papa.louis59@gmail.com>
2022-01-30 02:45:09 +0100

    Init with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+    // Pass-through files
+    eleventyConfig.addPassthroughCopy({ 'src/static/img': '/static/img' })
+    eleventyConfig.addPassthroughCopy({ 'src/static/css': '/static/css' })
+    eleventyConfig.addPassthroughCopy({ 'src/static/fonts': '/static/fonts' })
+    eleventyConfig.addPassthroughCopy({ 'src/static/js': '/static/js' })
+    eleventyConfig.addPassthroughCopy({ 'src/static/scss': '/static/scss' })
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
---
+++ src/static/js/animation.js
@@ -0,0 +1,36 @@
+
+window.onload = function () {
+
+    function navigateToSections(){
+        var nextButton = document.getElementById('fixedbutton');
+
+        var sections = ["home", "about", "experiences", "skills", "hobbies", "contact"]
+    
+        nextButton.addEventListener('click', event => {
+            var location = window.location.href.split("#")[1]
+            
+            if (!sections.includes(location)) {
+                window.location.href += "#about";
+                return;
+            }
+    
+            var index = sections.indexOf(location);
+            
+            var navTo = sections[++index];
+
+            if (navTo == undefined) {
+                window.location.href = window.location.href.split("#")[0] + "#home";
+                nextButton.textContent = "Next";
+                return;
+            }
+
+            window.location.href = window.location.href.split("#")[0] + "#" + navTo;
+
+            if (window.location.href.split("#")[1] == "contact") {
+                nextButton.textContent = "Top";
+            }
+        });
+    }
+
+    navigateToSections();
+}
