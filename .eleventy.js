0a6ce6cc4b2dbb6fe8dc45f5d53dcf3c1dcc9e97
PL Sergent <papa.louis59@gmail.com>
2024-02-18 18:27:04 +0100

    gallery built

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fg = require("fast-glob");
+
 module.exports = function (eleventyConfig) {
     // Pass-through files
     eleventyConfig.addPassthroughCopy({ 'src/static/img': '/static/img' })
@@ -6,7 +8,12 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'src/static/js': '/static/js' })
     eleventyConfig.addPassthroughCopy({ 'src/static/scss': '/static/scss' })
     eleventyConfig.addPassthroughCopy({ 'src/static/files': '/static/files' })
+    eleventyConfig.addPassthroughCopy({ 'src/_data/gallery': '/gallery/' })
     eleventyConfig.addPassthroughCopy({ 'src/ads.txt': '/ads.txt' })
+
+    eleventyConfig.addFilter("gallery", function (dir="") {
+        return fg.sync(dir);
+    });
 
     return {
         dir: {
