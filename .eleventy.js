a98e8cb8bfd7871368fc0991fc76e4f8495839f0
PL Sergent <papa.louis59@gmail.com>
2024-06-21 20:12:30 +0200

    Mobile view in order & images swap

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,30 @@ module.exports = function (eleventyConfig) {
             return imgPath.replace('/gallery/', 'src/static/img/gallery/');
         }
     });
+    eleventyConfig.addFilter("gallery_mobile", function (dir="") {
+        var imagesArr = fg.sync(dir)
+        var cols = [[], [], []]
+        var lengths = [0, 0, 0]
+
+        for (let i = 0; i < imagesArr.length; i++) {
+            var img = String(imagesArr[i])
+            galleryName = img.split("/")[4] // galleryX
+            galleryNumber = parseInt(galleryName.split("gallery")[1], 10) - 1
+            imgPath = img.replace(`src/static/img/gallery/gallery${galleryNumber+1}/`, '/gallery/photos/')
+            cols[galleryNumber].push(img)
+            lengths[galleryNumber] ++
+        }
+
+        maxLength = Math.max(...lengths)
+        const isMaxNumber= (element) => element == maxLength;
+        maxLengthIndex = lengths.findIndex(isMaxNumber)
+ 
+        result = []
+        for (let k = 0; k < cols[maxLengthIndex].length; k++) {
+            result.push(cols[0][k], cols[1][k], cols[2][k])
+        }
+        return result
+    });
 
     return {
         dir: {
